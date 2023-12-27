037b83ff808bcab20a31012fa5054c7c90914872
Piyush Thaware <piyushbthaware@gmail.com>
2023-12-27 22:05:40 +0530

    skills

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Projects from './components/Projects';
 import Contact from "./components/Contact";
+import Skills from './components/Skills';
 
 export default function App() {
 
@@ -20,14 +21,14 @@ export default function App() {
 
   return (
     // // bg-gray-950 
-    <div className='bg-[#272626] '>     
+    <div className='bg-[#272626] '>
       <Navbar toggleContactModel={toggleContactModel} />
       <Header />
       {/* <Projects/> */}
-
       {
-        showContactModel ? <Contact toggleContactModel={toggleContactModel}  /> : ''
+        showContactModel ? <Contact toggleContactModel={toggleContactModel} /> : ''
       }
+      <Skills/>
     </div>
   )
 }
