aa46335381b6fa971cf700bd037a1ce05d723229
Piyush Thaware <piyushbthaware@gmail.com>
2023-12-28 18:26:51 +0530

    MongoDB Atlas connected

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,18 @@
+/*
 const mongoose = require("mongoose");
 const dbName = "portfolio_contact";
-const url = `mongodb://127.0.0.1:27017/${dbName}`;
+const url = `mongodb://127.0.0.1:27017/${dbName}`; // Offline
 mongoose.connect(url);
 console.log("MongoDB is connected");
+*/
+
+
+// Online
+const mongoose = require("mongoose");
+const dbName = "portfolio_contact_online";
+const url = `mongodb+srv://piyushbthaware2:KHIfWxK3t0AYvD7p@cluster0.pohskju.mongodb.net/${dbName}?retryWrites=true&w=majority`; // Online
+mongoose.connect(url).then(() => {
+    console.log("Online MongoDB Connected Successfully...");
+}).catch((error) => {
+    console.log("Error : ", error);
+})
