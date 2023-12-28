2e1dbc23f50eb8f3e43a8ba030dfba1ac19ba4b9
Piyush Thaware <piyushbthaware@gmail.com>
2023-12-28 15:45:37 +0530

    contact form handle

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 // IMPORT ICONS
 import { RiMailSendFill } from "react-icons/ri";
 import { GrSend } from "react-icons/gr";
 import { IoClose } from "react-icons/io5";
 
 export default function Contact(props) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(name, email, message);
+    }
     return (
         <>
             <div className='main-contact fixed top-0 bottom-0 left-0 right-0 bg-[#0f0f0fc8]' style={{ fontFamily: "'Poppins', sans-serif" }}></div>
@@ -15,14 +23,14 @@ export default function Contact(props) {
                             <h1 className='text-lg text-white font-mono capitalize'>Get In <span className='text-[#47fffc]'>Touch</span> With Me</h1>
                             <button type='button' className='h-[30px] w-[30px] flex flex-nowrap items-center justify-center text-[#47fffc] text-2xl border-0 outline-none'><IoClose className='border-0 outline-none' onClick={() => props.toggleContactModel()} /></button>
                         </div>
-                        <div className="mt-10 text-white">
-                            <input type="text" placeholder='FULL NAME' className='border-b-[1.5px] border-gray-200/20 bg-transparent focus:outline-none ps-2 pb-3 mb-7 w-full font-[600] text-sm' required />
+                        <form className="mt-10 text-white" onSubmit={handleSubmit}>
+                            <input type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder='FULL NAME' className='border-b-[1.5px] border-gray-200/20 bg-transparent focus:outline-none ps-2 pb-3 mb-7 w-full font-[600] text-sm' required />
                             <br />
-                            <input type="text" placeholder='@EMAIL' className='border-b-[1.5px] border-gray-200/20 bg-transparent focus:outline-none ps-2 pb-3 mb-7 w-full font-[600] text-sm' required />
+                            <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder='@EMAIL' className='border-b-[1.5px] border-gray-200/20 bg-transparent focus:outline-none ps-2 pb-3 mb-7 w-full font-[600] text-sm' required />
                             <br />
-                            <textarea className='border-b-[1.5px] border-gray-200/20 bg-transparent focus:outline-none ps-2 pb-3 mb-7 w-full h-[80px] font-[600] text-sm' placeholder='MESSAGE'></textarea>
+                            <textarea value={message} onChange={(event) => setMessage(event.target.value)} className='border-b-[1.5px] border-gray-200/20 bg-transparent focus:outline-none ps-2 pb-3 mb-7 w-full h-[80px] font-[600] text-sm' placeholder='MESSAGE'></textarea>
                             <button type='submit' className='px-3 py-2 bg-[#47fffccf] rounded-sm text-sm font-mono flex flex-nowrap items-center gap-2 '>Send Message <GrSend className='text-lg' /></button>
-                        </div>
+                        </form>
 
                     </div>
                 </div>
