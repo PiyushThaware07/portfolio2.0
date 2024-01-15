bb90aa55caafebab7443370ca8eac00f28c8877c
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-15 16:01:19 +0530

    New Backend

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require("dotenv").config()
 const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
     response.send("Server Started");
 })
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server Started at port ", process.env.PORT);
--- Backend/router/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-// Import Controllers =================================
-const contactController = require("../controller/contactController");
-
-router.route("/contact").post(contactController);
-
-module.exports = router;
