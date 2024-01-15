111d149d6a5c5a29464c8b577e2dd944bb7dc6c1
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-15 16:34:02 +0530

    Backend .env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
     response.send("Server is started");
 })
+app.get("/about", (request, response) => {
+    response.send("about page");
+})
 
-app.listen(8000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server is started on port 8000");
 })
