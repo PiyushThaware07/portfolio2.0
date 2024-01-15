e090e6d3251575dfee895571f050b7734e87b08a
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-16 01:37:22 +0530

    Experience section fix

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // ICONS
 import { MdCardTravel } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
+// AOS ANIMATION
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
+
 
 
 export default function Experience() {
+
+    /*
+    useEffect(() => {
+        document.addEventListener("DOMContentLoaded", () => {
+            const timelineCard = document.querySelectorAll(".timeline-card");
+            console.log(timelineCard);
+            timelineCard.forEach((element, index) => {
+                // Checking if the index is even or odd
+                if (index % 2 === 0) {
+                    // Adding a custom attribute for even elements
+                    element.setAttribute("data-aos", "fade-right");
+                } else {
+                    // Adding a custom attribute for odd elements
+                    element.setAttribute("data-aos", "fade-left");
+                }
+            });
+        })
+    }, [])
+    */
+
+
     return (
         <>
             <div className="px-6 sm:px-10 md:px-16  pb-10">
@@ -15,40 +41,47 @@ export default function Experience() {
                 </div>
 
                 <div className="timelineContainer w-full h-[100%] flex flex-nowrap items-center justify-center relative z-2 after:bg-slate-900">
-                    <ul className="timeline-items w-[100%] md:w-[80%] mx-auto ps-10 md:ps-0">
+                    <ul className="timeline-items w-[100%] md:w-[80%] mx-auto ps-10 md:ps-0 ">
 
-                        <li className="timeline-item w-[100%]  md:w-[50%] shadow-md bg-slate-100 rounded-lg px-5 py-10 mb-5 text-slate-800 relative z-10 my-8 ">
-                            <h1 className='font-bold capitalize'>@Bootcoding PVT LTD</h1>
-                            <h1 className='font-semibold text-[13px] text-slate-600'>Software Developer Intern (MERN)</h1>
-                            <h1 className='font-semibold text-[13px] text-slate-600'>6 Months</h1>
-                            <h1 className='font-semibold text-[13px] text-slate-600'>~ Currently working on codelab</h1>
-                            <span className='location bg-white text-slate-900 px-2 py-1 rounded-lg text-sm font-semibold inline-flex items-center absolute bottom-2 right-2'
-                            style={{boxShadow:"inset 0 0 4px rgba(0, 0, 0, 0.2)"}}>
-                                <FaLocationDot className='' />
-                                <span className='text-[13px] font-semibold'>Nagpur</span>
-                            </span>
-                            <span className="circle w-32 absolute flex flex-nowrap items-center justify-center gap-2 bg-slate-900 px-3 py-1 rounded-full">
+                        <li className="timeline-item w-[100%]  md:w-[50%]  cursor-pointer relative z-[9] mt-8 md:mt-0 md:pt-8 mb-5" >
+                            <div className="timeline-card relative w-full h-auto shadow-md bg-slate-100 rounded-lg px-5 py-10  text-slate-800" >
+                                <h1 className='font-bold capitalize '>@Bootcoding PVT LTD</h1>
+                                <h1 className='font-semibold text-[13px] text-slate-600'>Software Developer Intern (MERN)</h1>
+                                <h1 className='font-semibold text-[13px] text-slate-600'>6 Months</h1>
+                                <h1 className='font-semibold text-[13px] text-slate-600'>~ Currently working on codelab</h1>
+                                <span className='location bg-white text-slate-900 px-2 py-1 rounded-lg text-sm font-semibold inline-flex items-center absolute bottom-2 right-2'
+                                    style={{ boxShadow: "inset 0 0 4px rgba(0, 0, 0, 0.2)" }}>
+                                    <FaLocationDot className='' />
+                                    <span className='text-[13px] font-semibold'>Nagpur</span>
+                                </span>
+                            </div>
+                            <span className="circle w-32 absolute flex flex-nowrap items-center justify-center gap-2 bg-slate-900 px-3 py-1 rounded-full z-[999]">
                                 <span className='icon text-white rounded-full '><MdCardTravel /></span>
                                 <span className='duration text-sm text-white font-semibold' >2020 - 2021</span>
                             </span>
                         </li>
-                        <li className="timeline-item w-[100%]  md:w-[50%] shadow-md bg-slate-100 rounded-lg px-5 py-10 mb-5 text-slate-800 relative z-10 my-8 ">
-                            <h1 className='font-bold capitalize'>@Bootcoding PVT LTD</h1>
-                            <h1 className='font-semibold text-[13px] text-slate-600'>Software Developer Intern (MERN)</h1>
-                            <h1 className='font-semibold text-[13px] text-slate-600'>6 Months</h1>
-                            <h1 className='font-semibold text-[13px] text-slate-600'>~ Currently working on codelab</h1>
-                            <span className='location bg-white text-slate-900 px-2 py-1 rounded-lg text-sm font-semibold inline-flex items-center absolute bottom-2 right-2'
-                            style={{boxShadow:"inset 0 0 4px rgba(0, 0, 0, 0.2)"}}>
-                                <FaLocationDot className='' />
-                                <span className='text-[13px] font-semibold'>Nagpur</span>
-                            </span>
-                            <span className="circle w-32 absolute flex flex-nowrap items-center justify-center gap-2 bg-slate-900 px-3 py-1 rounded-full">
+                        <li className="timeline-item w-[100%]  md:w-[50%]  cursor-pointer relative z-[9] mt-8 md:mt-0 md:pt-8" >
+                            <div className="timeline-card relative w-full h-auto shadow-md bg-slate-100 rounded-lg px-5 py-10  text-slate-800" >
+                                <h1 className='font-bold capitalize '>@Bootcoding PVT LTD</h1>
+                                <h1 className='font-semibold text-[13px] text-slate-600'>Software Developer Intern (MERN)</h1>
+                                <h1 className='font-semibold text-[13px] text-slate-600'>6 Months</h1>
+                                <h1 className='font-semibold text-[13px] text-slate-600'>~ Currently working on codelab</h1>
+                                <span className='location bg-white text-slate-900 px-2 py-1 rounded-lg text-sm font-semibold inline-flex items-center absolute bottom-2 right-2'
+                                    style={{ boxShadow: "inset 0 0 4px rgba(0, 0, 0, 0.2)" }}>
+                                    <FaLocationDot className='' />
+                                    <span className='text-[13px] font-semibold'>Nagpur</span>
+                                </span>
+                            </div>
+                            <span className="circle w-32 absolute flex flex-nowrap items-center justify-center gap-2 bg-slate-900 px-3 py-1 rounded-full z-[999]">
                                 <span className='icon text-white rounded-full '><MdCardTravel /></span>
                                 <span className='duration text-sm text-white font-semibold' >2020 - 2021</span>
                             </span>
                         </li>
-                       
                         
+                      
+
+
+
 
 
 
