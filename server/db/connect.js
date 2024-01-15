8c1a6f213119c61a1cbe36f6da8bcff8fdf285b2
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-15 22:50:58 +0530

    database fix

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,14 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-
+const dbURL = process.env.HOST_ENV == "production" ? process.env.ONLINE_DATABASE : process.env.OFFLINE_DATABASE
 
 function connectDB() {
-    const connect = mongoose.connect(
-        process.env.OFFLINE_DATABASE
-        ||
-        process.env.ONLINE_DATABASE
-    )
-    console.log("Database is connected", process.env.OFFLINE_DATABASE || process.env.ONLINE_DATABASE);
+    const connect = mongoose.connect(dbURL)
+    console.log("Database is connected", dbURL);
     return connect;
 }
 
