c1cec20719bcd0de175d09b57ac2a04e7f696c0d
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-15 15:55:00 +0530

    Backend

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,22 +1,11 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-// const cors = require("cors");
-// Import Router =====================================
-const router = require("./router/router");
-// Middleware
-// app.use(cors());
-// app.use(express.json())
 
-// Home Route
-app.get('/', (request, response) => {
-    response.send("Server is working");
+app.get("/", (request, response) => {
+    response.send("Server Started");
 })
-// Routers
-// app.use("/api/", router);
 
-
-const port = 8000;
-app.listen(port, () => {
-    console.log(`Server Started at port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log("Server Started at port ", process.env.PORT);
 })
