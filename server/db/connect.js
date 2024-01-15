42ec0bff5c00acd91094d5a68cf050c60dae956a
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-15 23:07:08 +0530

    vercel env

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const dbURL = process.env.HOST_ENV == "production" ? process.env.ONLINE_DATABASE : process.env.OFFLINE_DATABASE
+// const dbURL = process.env.HOST_ENV == "production" ? process.env.ONLINE_DATABASE : process.env.OFFLINE_DATABASE
 
 function connectDB() {
-    const connect = mongoose.connect(dbURL)
-    console.log("Database is connected", dbURL);
+    const connect = mongoose.connect(process.env.ONLINE_DATABASE)
+    console.log("Database is connected");
     return connect;
 }
 
