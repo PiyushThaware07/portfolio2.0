646c1b0ccea63aa73e1ea534fb9dd789e10800bc
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-17 00:52:52 +0530

    About data added

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -10,6 +10,7 @@ import resume from "../assets/img/resume.png";
 
 
 export default function Resume() {
+
     return (
         <>
             <div className="px-0 sm:px-10 md:px-16 pb-10">
@@ -22,15 +23,16 @@ export default function Resume() {
                     <div className="about p-7 sm:p-10">
                         <h1 className='text-4xl sm:text-5xl font-bold capitalize text-slate-950'>Hey <span className='text-blue-600' >There !</span></h1>
                         <p className='mt-5 mb-2 text-md font-semibold text-slate-500'>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eaque ipsum rem consequuntur deserunt ex! Labore.
+                            Hello there! I'm <span className='text-slate-700' >Piyush Thaware</span>, a final-year <span className='text-slate-700' >Computer Science Engineering student</span> with a deep interest in designing and programming. My journey involves continuous learning, with a commitment to evolving as a <span className='text-slate-700' >proficient designer and programmer</span>. Currently navigating the challenges of my <span className='text-slate-700' >B.Tech. (CSE) final year</span>, I'm enthusiastic about bridging the gap between my creative interests and technical skills. Beyond the digital landscape, you'll often find me sketching away, channeling my creativity into visual art.
                         </p>
-                        <p className='mt-5 mb-2 text-md font-semibold text-slate-500'>
-                            molestias architecto, dolorem quisquam voluptatibus nisi dignissimos qui quia, adipisci a quos? Fuga quis quaerat, repellendus quia, incidunt illum fugiat nemo possimus reprehenderit iste doloribus cupiditate, a excepturi repellat. Eveniet optio doloribus maxime delectus, nemo ad voluptate labore, ipsam hic at iure, totam laudantium.
+                        <p className='mt-5 mb-2 text-md font-semibold text-slate-500' >
+                            Join me as I embark on this exciting adventure of merging creativity and technology!
                         </p>
+
                         <h1 className='mt-5 mb-2 text-md font-bold text-slate-900'>Connect With Me.</h1>
                         <div className="flex flex-nowrap items-center gap-3">
-                            <button type="button" className='px-8 py-3 text-white bg-gradient-to-r from-cyan-500 to-blue-500 font-semibold text-sm rounded-md flex flex-nowrap items-center gap-1' >Linkedin <FaLinkedin className='text-lg' /></button>
-                            <button type="button" className='px-8 py-3 text-white bg-gradient-to-r from-red-500 to-pink-500 font-semibold text-sm rounded-md flex flex-nowrap items-center gap-1' >Email <MdMarkEmailUnread className='text-lg' /></button>
+                            <a href='https://linkedin.com/in/piyush-thaware/' target='_blank' className='cursor-pointer  px-8 py-3 text-white bg-gradient-to-r from-cyan-500 to-blue-500 font-semibold text-sm rounded-md flex flex-nowrap items-center gap-1' >Linkedin <FaLinkedin className='text-lg' /></a>
+                            <a href="mailto:piyushbthaware@gmail.com?subject=Your%20Subject&body=Your%20Message" target="_blank" className='cursor-pointer  px-8 py-3 text-white bg-gradient-to-r from-red-500 to-pink-500 font-semibold text-sm rounded-md flex flex-nowrap items-center gap-1' >Email <MdMarkEmailUnread className='text-lg' /></a>
                         </div>
                     </div>
                     <div className="pdf border-[3px] border-slate-950 rounded-xl mx-5 sm:mx-10 md:m-10 min-h-[400px] sm:max-h-[500px] overflow-hidden relative">
