ee9c9767c5772af9c4c2372cbacfbbfbd705419c
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-21 21:03:05 +0530

    Project backend remove

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 // & RRD =================================
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from './dashboard/Dashboard.jsx';
 
 const backendURL = import.meta.env.VITE_REACT_APP_HOST_ENV == "production" ? import.meta.env.VITE_REACT_APP_BACKEND_ONLINE : import.meta.env.VITE_REACT_APP_BACKEND_OFFLINE
 
@@ -13,8 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        {/* <Route path='/' element={<App />} /> */}
-        <Route path='/' element={<Dashboard backendURL={backendURL} />} />
+        <Route path='/' element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>,
--- server/router/router.js
+++ server/router/router.js
@@ -3,19 +3,12 @@ const router = express.Router();
 // Import Controllers ===================================================
 const contactController = require("../controller/contactController");
 const { insertExperienceController, retriveExperienceController } = require("../controller/experienceController");
-const { insertProjectController, retriveProjectController } = require("../controller/projectController");
-// Import Middleware =====================================================
-const uploadMiddleware = require("../middleware/uploadMiddleware");
-
 
 // * CRUD on contact
 router.route("/contact").post(contactController);
 // * CRUD on experience
 router.route("/insertExperience").post(insertExperienceController);
 router.route("/retriveExperience").get(retriveExperienceController);
-// * CRUD on project
-router.route("/insertProject").post(uploadMiddleware, insertProjectController);
-router.route("/retriveProject").get(retriveProjectController);
 
 
 
