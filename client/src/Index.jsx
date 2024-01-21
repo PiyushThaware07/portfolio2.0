ed401882c076aea269e650b881e3ad7ce6dbe3ab
Piyush Thaware <piyushbthaware@gmail.com>
2024-01-21 20:19:50 +0530

    Project Experience Backend

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -13,8 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/dashboard' element={<Dashboard backendURL={backendURL} />} />
+        {/* <Route path='/' element={<App />} /> */}
+        <Route path='/' element={<Dashboard backendURL={backendURL} />} />
       </Routes>
     </Router>
   </React.StrictMode>,
